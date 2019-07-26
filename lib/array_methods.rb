590780fb3cfa71634e751ed9e22d810be653e44c
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 01:06:05 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  scale = [2, 3, 4, 5]
+  scale.each do |number, index|
+    puts "Moving on to index number #{2}"
+    puts "The current number is #{number}"
+  end
+scale = [2, 3, 4, 5]
 
 def find_max_value(array)
-  # Add your solution here
+scale_2 = [6,5,4,3]
+  p array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+valley = [5,4,3,2,1,0,1,2,3,4,5]
+  p array.min
 end
